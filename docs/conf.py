a8fb5662fffa2ed7f68958d97374a6c3fcabdb1e
maurya-anand <anandmaurya@hotmail.com>
2023-06-15 12:57:03 +0100

    hotfic for readthedocs error

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,6 @@
 
 import os
 import sys
-import list_packages
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
