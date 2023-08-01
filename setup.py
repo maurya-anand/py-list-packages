b310fec852888dabbb885d54fd410cbb3746f8eb
maurya-anand <anandmaurya@hotmail.com>
2023-08-01 11:42:45 +0100

    bumping version to test the markdown description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='list_packages',
-    version='1.0.1',
+    version='1.0.2',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Anand Maurya',
